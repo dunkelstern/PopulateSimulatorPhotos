d412e9bae51f94ae4b04144477a11170b0686509
Justin Williams <justin@secondgearsoftware.com>
2013-03-21 15:39:21 -0600

    Initial Commit

---
+++ PopulatePhotos/SGViewController.h
@@ -0,0 +1,13 @@
+//
+//  SGViewController.h
+//  PopulatePhotos
+//
+//  Created by Justin Williams on 3/21/13.
+//  Copyright (c) 2013 Second Gear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGViewController : UIViewController
+
+@end
---
+++ PopulatePhotos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PopulatePhotos
+//
+//  Created by Justin Williams on 3/21/13.
+//  Copyright (c) 2013 Second Gear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SGAppDelegate class]));
+    }
+}
